ee741e59a3695c9da9d8df1af1ff9cfaca5ce514
A-D-11 <104553483+A-D-11@users.noreply.github.com>
2022-05-21 18:07:28 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Modal from "react-bootstrap";
-import { FcGoogle } from "react-icons/fc";
-import { FaTwitter, FaFacebook, FaGithub } from "react-icons/fa";
-import { Button } from "bootstrap";
 import { useState } from 'react';
 import { Tabs, Tab, Typography, Box, Paper} from '@mui/material';
 import Login from './Login';
+import SignInUp from "./SignInUp";
   
 export default function Header() {
 
@@ -134,11 +131,6 @@ export default function Header() {
                   Login
                 </button>
               </a>
-          {/* <a href="">
-                <button type="button" onClick={newsignup()} className="btn btn-warning">
-                  Sign-up
-                </button>
-              </a> */}
             </div>
           )}
           <div>
@@ -150,117 +142,25 @@ export default function Header() {
             >
               Sign-In
             </button>
-            <div
-              className="modal fade"
-              id="staticBackdrop"
-              data-bs-backdrop="static"
-              data-bs-keyboard="false"
-              tabIndex="-1"
-              aria-labelledby="staticBackdropLabel"
-              aria-hidden="true"
-            >
+            <div className="modal fade"
+              id="staticBackdrop"data-bs-backdrop="static"
+              data-bs-keyboard="false"tabIndex="-1"
+              aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
                   <Paper style={paStyled}>
-      <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
-        <Tab label="Sign-In"  />
-        <Tab label="Sign-Up" />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-           <Login />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-      <Paper elevation={15} style={pStyle} >
-      <div className="modal-body">
-                    <form className="">
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-
-                      <div className="form-floating mb-1">
-                        
-                        <input
-                          type="text"
-                          className="form-control rounded-4"
-                          placeholder="Tony Stark"
-                        />
-                        <label htmlFor="floatingInput">User Name</label>
-                      </div>
-                      <div className="form-floating mb-1">
-                        <input
-                          type="email"
-                          className="form-control rounded-4"
-                          id="floatingInput"
-                          placeholder="name@example.com"
-                        />
-                        <label htmlFor="floatingInput">Email address</label>
-                      </div>
-                      <div className="form-floating mb-1">
-                        <input
-                          type="password"
-                          className="form-control rounded-4"
-                          id="floatingPassword"
-                          placeholder="Password"
-                        />
-                        <label htmlFor="floatingPassword">Password</label>
-                      </div>
-                      <div className="form-floating mb-3">
-                        <input
-                          type="password"
-                          className="form-control rounded-4"
-                          id="floatingPassword"
-                          placeholder="Password"
-                        />
-                        <label htmlFor="floatingPassword">
-                          Confirm Password
-                        </label>
-                      </div>
-                      <button
-                        className="w-100 mb-2 btn btn-lg rounded-4 btn-primary"
-                        type="submit"
-                      >
-                        Sign up
-                      </button>
-                      <small className="text-muted">
-                        By clicking Sign up, you agree to the terms of use.
-                      </small>
-                      <hr className="my-4" />
-                      <h4 className="fs-5 fw-bold mb-3">
-                        Continue another way
-                      </h4>
-                      <button
-                        className="w-100 py-2 mb-2 btn btn-outline-dark rounded-4"
-                        type="submit"
-                      >
-                        <FcGoogle className="mr-icons" />
-                        Sign up with Google
-                      </button>
-                      <button
-                        className="w-100 py-2 mb-2 btn btn-outline-dark rounded-4"
-                        type="submit"
-                      >
-                        <FaTwitter className="mr-icons" />
-                        Sign up with Twitter
-                      </button>
-                      <button
-                        className="w-100 py-2 mb-2 btn btn-outline-primary rounded-4"
-                        type="submit"
-                      >
-                        <FaFacebook className="mr-icons" />
-                        Sign up with Facebook
-                      </button>
-                      <button
-                        className="w-100 py-2 mb-2 btn btn-outline-secondary rounded-4"
-                        type="submit"
-                      >
-                        <FaGithub className="mr-icons" />
-                        Sign up with GitHub
-                      </button>
-                    </form>
-                  </div>
-                
-                  </Paper>
-      </TabPanel>
-      </Paper>
+                   <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
+                   <Tab label="Sign-In"  />
+                   <Tab label="Sign-Up" />
+                   </Tabs>
+                   <TabPanel value={value} index={0}>
+                      <Login />
+                   </TabPanel>
+                   <TabPanel value={value} index={1}>
+                   <SignInUp />
+                   </TabPanel>
+                   </Paper>
                   </div>
                   </div>
               </div>
